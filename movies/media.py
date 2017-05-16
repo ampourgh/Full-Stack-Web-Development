d67ce5ea7783673eec6b5c23c63756597297729f
ampourgh <ampourgh@gmail.com>
2017-05-16 12:58:36 -0600

    Update media.py

--- movies/media.py
+++ movies/media.py
@@ -1,7 +1,9 @@
 import webbrowser
 
+# Video parent class 
 class Video():
     """This is the Video parent class that houses movie and perhaps other future mediums """
+    #constructor for instance variables title, year and duration
     def __init__(self,
                  title,
                  year,
@@ -10,21 +12,24 @@ class Video():
         self.year = year
         self.duration = duration
 
+# Movie child class that inherits from Video
 class Movie(Video):
     """ This class provides a way to store movie related information """ #__doc__
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"] #a constant - the movie ratings
     
+    # Movie's constructor variable with Video's inherited variable, along with Movie specific instance variables
     def __init__(self,
                  title,
                  year,
                  duration,
                  movie_storyline,
                  poster_image,
-                 trailer_youtube): #constructor = the __init__ initializer
-        Video.__init__(self, title, year, duration) #inheritance
-        self.storyline = movie_storyline #self. makes it an instance variable
+                 trailer_youtube):
+        Video.__init__(self, title, year, duration) #inheritance from Video
+        self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    def show_trailer(self): #instance method
+    # instance method which defines show_trailer
+    def show_trailer(self): 
         webbrowser.open(self.trailer_youtube_url)
