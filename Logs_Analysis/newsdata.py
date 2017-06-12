297056e0d58fd2889c6c3b74c815a9bf73ab86ab
ampourgh <ampourgh@gmail.com>
2017-06-12 12:35:15 -0500

    Create newsdata.py

--- Logs_Analysis/newsdata.py
+++ Logs_Analysis/newsdata.py
@@ -1,7 +1,12 @@
-import psycopg2, decimal
+#!/usr/bin/env python3
+
+import psycopg2
+import decimal
+
 
 # the database within
 DBNAME = "news"
+
 
 def toparticles():
     # Connect to database
@@ -31,16 +36,20 @@ def toparticles():
     db.close()
     return posts
 
+
 def topauthors():
     db = psycopg2.connect(dbname=DBNAME)
     c = db.cursor()
     query2 = """ create view topauthors as
                     select authors.name, count(log.path) as num
                     from articles, log, authors
-                    where '/article/' || articles.slug = log.path and articles.author = authors.id and authors.name != 'Anonymous Contributor'
+                    where '/article/' || articles.slug = log.path
+                            and articles.author = authors.id
+                            and authors.name != 'Anonymous Contributor'
                     group by name
                     order by num desc
                     limit 3;
+
 
                 select * from topauthors;"""
     c.execute(query2)
@@ -48,18 +57,21 @@ def topauthors():
     db.close()
     return posts2
 
+
 def errorsperc():
     db = psycopg2.connect(dbname=DBNAME)
     c = db.cursor()
     query3 = """create view Error as
-                    select substring(CAST (time AS text) from 1 for 10) as date, cast(count(status) as float) as errors
+                    select substring(CAST (time AS text) from 1 for 10)
+                            as date, cast(count(status) as float) as errors
                     from log
                     where status = '404 NOT FOUND'
                     group by date
                     order by date desc;
 
                 create view Total as
-                    select substring(CAST (time AS text) from 1 for 10) as date2, cast(count(status) as float) as totals
+                    select substring(CAST (time AS text) from 1 for 10)
+                            as date2, cast(count(status) as float) as totals
                     from log
                     group by date2
                     order by date2 desc;
@@ -74,14 +86,15 @@ def errorsperc():
     return posts3
 
 
-#headline before ranking
+# headline before ranking
 print('Top 3 Most Popular Articles')
 num = 1
 # loops through each row in the query
 for row in toparticles():
 
     # row[0] and row[1] being the first and second column, respectively
-    stat = str(num) + '.) ' + str(row[0]) + ": " + str(('{:,}'.format(row[1]))) + ' views'
+    stat = (str(num) + '.) ' + str(row[0]) + ": " +
+            str(('{:,}'.format(row[1]))) + ' views')
 
     # Increments num for its position in the ranking
     num += 1
@@ -90,7 +103,8 @@ for row in toparticles():
 print('\nTop 3 Most Popular Authors')
 num2 = 1
 for row in topauthors():
-    stat = str(num2) + '.) ' + str(row[0]) + ": " + str(('{:,}'.format(row[1]))) + ' views'
+    stat = (str(num2) + '.) ' + str(row[0]) + ": " +
+            str(('{:,}'.format(row[1]))) + ' views')
     print(stat)
     num2 += 1
 
@@ -99,5 +113,6 @@ for row in errorsperc():
 
     # Imported decimal to round to the nearest hundredth
     # % and error are separate strings, caused my laptop to crash when combined
-    stat = str(row[0]) + ": " + str(round(decimal.Decimal(row[1]), 2)) + '%' + ' errors'
+    stat = (str(row[0]) + ": " + str(round(decimal.Decimal(row[1]), 2)) +
+            '%' + ' errors')
     print(stat)
