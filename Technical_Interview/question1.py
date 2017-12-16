0ffb94aa6da47ea562c5b507697db488bc56d36a
Amir Pourghaed <ampourgh@gmail.com>
2017-12-16 12:46:13 -0600

    feat: remove duplicates and case sensitivity
    The search is no long case sensitive and discards duplicates added to new_string_s.

--- Technical_Interview/question1.py
+++ Technical_Interview/question1.py
@@ -13,10 +13,13 @@ def question1(s, t):
     # Put all shared letters between s and t in var below
     new_string_s = ""
     for s_letter in s:
+        done = 'not done'
         t_count = 1
         for  t_letter in t:
-            if s_letter == t_letter:
+            if (s_letter.lower() == t_letter.lower() and
+               done != 'done'):
                 new_string_s += s_letter
+                done = 'done'
             elif t_count == len(t):
                 return False
             else:
@@ -24,11 +27,15 @@ def question1(s, t):
 
     # If s and the new var are the same,
     # then all letters in s are in t
-    if new_string_s == s:
+    if new_string_s.lower() == s:
+        print 'True'
         return True
     else:
+        print new_string_s.lower()
         return False
 
 # check if each letter in the first string
 # is the same as the letters in the second string
 question1('ad', 'udacity')
+question1('pdf', 'portable document format')
+question1('gif', 'Graphics Interchange Format i')
