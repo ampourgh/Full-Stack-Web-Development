06ef77ee2801010cfde2145877479b72a6d0fcf3
Amir Pourghaed <ampourgh@gmail.com>
2017-12-18 01:42:30 -0600

    Refactor: reduce to 1 loop with more conditions
    There previously was a nested loop which has since been reduced to a single loop going through the original phrase. If the function begins to see a pattern, the conditions increment/decrement on either side of a letter to find the longest palindrome.

--- Technical_Interview/question2.py
+++ Technical_Interview/question2.py
@@ -7,26 +7,73 @@ and return a string.
 """
 
 def question2(a):
-    # Initial placeholder string
+
+    # The initial increment
+    i = 1
+
+    # Creating the variable that will store the longest palindrom
     initial_string = 'hi'
+        
+    # Continues looping until false
+    while True:
+        
+        # i goes through each letter where
+        # j is a letter below, k a letter above
+        # and l being where i is
+        done = 'not done'
+        j = i - 1
+        k = i + 2
+        l = 0
+        
+        # The conditions prevent the increments to go out of range
+        while (j != -1 or
+                k != len(a) or
+                l != len(a) or
+                done != 'done'):
 
-    # Get the value and index of string 
-    for first_index, first_a_letter in enumerate(a):
-        for second_index, second_a_letter in enumerate(a):
-            
-            # Get a range of letters within the string
-            new_word = a[first_index:1 + second_index:]
+            # Creating strings from snippets of the original word
+            string_value = a[j:k].lower()
+            string_value2 = a[j:l].lower()
+            string_value3 = a[j:i + 1].lower()
 
-            # If range of letters are the same forward and backwards
-            # Change the initial string
-            if new_word.lower() == new_word[::-1].lower():
-                if len(new_word) > len(initial_string):
-                    initial_string = new_word
-                    
-    print initial_string
+            # This condition assumes there is a singular letter in the middle
+            if string_value == string_value[::-1]:
+                if len(string_value) > len(initial_string):
+                    initial_string = a[j:k]
+                j -= 1
+                k += 1
 
+            # The following conditions treat as though there's 2 letters
+            # that are the same in the middle
+            elif string_value2== string_value2[::-1]:
+                if len(string_value2) > len(initial_string):
+                    initial_string = a[j:i]
+                j -= 1
+                l += 1
+                
+            elif string_value3 == string_value3[::-1]:
+                if len(string_value3) > len(initial_string):
+                    initial_string = a[j:i]
+                j -= 1
+                l = i + 2
+
+            # Once found, print result and return false to get out of the loop
+            elif i == len(a):
+                print '\nResult:'
+                print initial_string
+                return False
+
+            # Else revert k and j to being a character above and below i
+            # and i moving up to the next letter
+            else:
+                done = 'done'
+                i += 1
+                k = i + 2
+                j = i - 1 
+    
 # Try the string 'racecara',
 # which should return all the letters besides the last a
-question2('racecara')
-question2('a Tacocat')
+# 'Taccat' is an exmaple of a word with 2 matching letters in the center
+question2('racecarat')
+question2('a Taccat')
 question2('How are you, mom?')
