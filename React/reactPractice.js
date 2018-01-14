dc901edc87c4024371c112d54dd9cd7de53c2f09
Amir Pourghaed <ampourgh@gmail.com>
2018-01-14 10:23:58 -0600

    Create reactPractice.js

---
+++ React/reactPractice.js
@@ -0,0 +1,17 @@
+// Ex. JSX contained within an object
+const myTeam = {
+  center: <li>Benzo Walli</li>,
+  powerForward: <li>Rasha Loa</li>,
+  smallForward: <li>Tayshaun Dasmoto</li>,
+  shootingGuard: <li>Colmar Cumberbatch</li>,
+  pointGuard: <li>Femi Billon</li>
+};
+
+// If JSX takes more than 1 line, put in parenthesis
+var myDiv = (
+  <div id = 'some-id'>
+    <h1>
+      Hello world
+    </h1>
+  </div>
+);
