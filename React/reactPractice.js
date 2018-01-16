db9527871bc698002fcd3950eb5339f1d6732338
Amir Pourghaed <ampourgh@gmail.com>
2018-01-16 17:10:51 -0600

    chore: progress on unit

--- React/reactPractice.js
+++ React/reactPractice.js
@@ -232,3 +232,62 @@ ReactDOM.render(
   <MyComponentClass />,
   document.getElementById('app')
 );
+
+/* -------------------------------------------*/
+
+// renders content within the parenthesis 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class QuoteMaker extends React.Component {
+  render() {
+    return (
+      <blockquote>
+        <p>
+          What is important now is to recover our senses.
+        </p>
+        <cite>
+          <a target="_blank" 
+            href="https://en.wikipedia.org/wiki/Susan_Sontag">
+            Susan Sontag
+          </a>
+        </cite>
+      </blockquote>
+    );
+  }
+};
+
+ReactDOM.render(
+  <QuoteMaker />,
+  document.getElementById('app')
+);
+
+/* -------------------------------------------*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const owl = {
+  title: 'Excellent Owl',
+  src: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-owl.jpg'
+};
+
+// Component class starts here:
+class Owl extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{owl.title}</h1>
+        <img src={owl.src}
+          	 alt={owl.title}/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Owl />, 
+                document.getElementById('app')
+)
+
+/* -------------------------------------------*/
