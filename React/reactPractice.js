bfb4d1727f59470a5d72c8ca14ed914965ae0ee2
Amir Pourghaed <ampourgh@gmail.com>
2018-01-15 20:09:12 -0600

    chore: add more tutorial progress

--- React/reactPractice.js
+++ React/reactPractice.js
@@ -94,4 +94,46 @@ ReactDOM.render(
   document.getElementById('app')
 );
 
+/* -------------------------------------------*/
+
+const pics = {
+  panda: "http://bit.ly/1Tqltv5",
+  owl: "http://bit.ly/1XGtkM3",
+  owlCat: "http://bit.ly/1Upbczi"
+}; 
+
+const panda = (
+  <img 
+    src={pics.panda} 
+    alt="Lazy Panda" />
+);
+
+const owl = (
+  <img 
+    src={pics.owl} 
+    alt="Unimpressed Owl" />
+);
+
+/* -------------------------------------------*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function makeDoggy(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+const kitty = (
+	<img 
+		src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg" 
+		alt="kitty" 
+    onClick={makeDoggy}/>
+);
+
+ReactDOM.render(kitty, document.getElementById('app'));
+
+/* -------------------------------------------*/
 
