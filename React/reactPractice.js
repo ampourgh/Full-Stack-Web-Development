90bc2e5f9335ec597ad0d8f1cbaf55a9e523138e
Amir Pourghaed <ampourgh@gmail.com>
2018-01-16 13:40:34 -0600

    chore: finish unit

--- React/reactPractice.js
+++ React/reactPractice.js
@@ -137,3 +137,77 @@ ReactDOM.render(kitty, document.getElementById('app'));
 
 /* -------------------------------------------*/
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function coinToss () {
+  // Randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg'
+};
+
+const img = <img src={pics[coinToss() === 'heads' ? 'doggy' : 'kitty']} />;
+
+ReactDOM.render(
+	img, 
+	document.getElementById('app')
+);
+
+/* -------------------------------------------*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// judgmental will be true half the time.
+const judgmental = Math.random() < 0.5;
+
+const favoriteFoods = (
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Sushi Burrito</li>
+      <li>Rhubarb Pie</li>
+      { !judgmental && <li>Nacho Cheez Straight Out The Jar</li> }
+      <li>Broiled Grapefruit</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(
+	favoriteFoods, 
+	document.getElementById('app')
+);
+
+/* -------------------------------------------*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+// the key is for knowing where the item is on a list
+// or when, for instance, the list needs to be reshuffled
+const peopleLis = people.map((person, i) =>
+  // expression goes here:
+	<li key={'person_' + i}>{person}</li>
+);
+
+// ReactDOM.render goes here:
+ReactDOM.render(<ul>{peopleLis}</ul>, 
+		document.getElementById('app'))
+		
+/* -------------------------------------------*/
+
+const greatestDivEver = React.createElement(
+  "div",
+  null,
+  "i am div"
+);
+
+// the code above translates to the following:
+// const greatestDivEver = <div>i am div</div>;
+/* -------------------------------------------*/
