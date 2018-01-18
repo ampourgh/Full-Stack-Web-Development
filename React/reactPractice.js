56ed0012fad3a164b3cd9a104f66914e2eca19dd
Amir Pourghaed <ampourgh@gmail.com>
2018-01-18 12:26:39 -0600

    chore: learn about stateless and stateful

--- React/reactPractice.js
+++ React/reactPractice.js
@@ -712,3 +712,30 @@ class Toggle extends React.Component {
 ReactDOM.render(<Toggle />, document.getElementById('app'));
 
 /* -------------------------------------------*/
+
+// STATELESS
+import React from 'react';
+
+export class Child extends React.Component {
+  render() {
+    return <h1>Hey, my name is {this.props.name}!</h1>;
+  }
+}
+
+// STATEFUL
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Child } from './Child'
+
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'Frarthur' };
+  }
+
+  render() {
+    return <Child name={this.state.name}/>;
+  }
+}
+
+ReactDOM.render(<Parent />, document.getElementById('app'));
