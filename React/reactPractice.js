ed777c0d299ec6611e7a310b0d8953b00e76177c
Amir Pourghaed <ampourgh@gmail.com>
2018-01-18 08:55:24 -0600

    chore: react toggle method

--- React/reactPractice.js
+++ React/reactPractice.js
@@ -676,3 +676,39 @@ class App extends React.Component {
 ReactDOM.render(<App />, document.getElementById('app'))
 
 /* -------------------------------------------*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const green = '#39D1B4';
+const yellow = '#FFD712';
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: green };
+    this.changeColor = this.changeColor.bind(this);
+  }
+  
+  changeColor() {
+    const changeColor = this.state.color == green ? yellow : green;
+    this.setState({ color: changeColor });
+  }
+  
+  render() {
+    return (
+      <div style={{background: this.state.color}}>
+        <h1>
+          Change my color
+        </h1>
+        <button onClick={this.changeColor}>
+          Change color
+        </button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Toggle />, document.getElementById('app'));
+
+/* -------------------------------------------*/
