fa025caec84990271e0300c71fe9fb2d50aa1ffc
Amir Pourghaed <ampourgh@gmail.com>
2018-01-20 10:11:52 -0600

    chore: tutorial on containers and components

--- React/reactPractice.js
+++ React/reactPractice.js
@@ -911,3 +911,74 @@ ReactDOM.render(
 );
 
 /* -------------------------------------------*/
+// ./components/GuineaPigs.js
+import React from 'react';
+
+const GUINEAPATHS = [
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-1.jpg',
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-2.jpg',
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-3.jpg',
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-4.jpg'
+];
+
+export class GuineaPigs extends React.Component {
+  render() {
+    const src = this.props.src;
+    return (
+      <div>
+        <h1>Cute Guinea Pigs</h1>
+        <img src={src} />
+      </div>
+    );
+  }
+}
+
+// ./containers/GuineaPigsContainer.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GuineaPigs } from '../components/GuineaPigs.js'
+
+const GUINEAPATHS = [
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-1.jpg',
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-2.jpg',
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-3.jpg',
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-4.jpg'
+];
+
+class GuineaPigsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { currentGP: 0 };
+
+    this.interval = null;
+
+    this.nextGP = this.nextGP.bind(this);
+  }
+
+  nextGP() {
+    let current = this.state.currentGP;
+    let next = ++current % GUINEAPATHS.length;
+    this.setState({ currentGP: next });
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.nextGP, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const src = GUINEAPATHS[this.state.currentGP];
+    return <GuineaPigs src={src}/>;
+  }
+}
+
+ReactDOM.render(
+  <GuineaPigsContainer />,
+  document.getElementById('app')
+);
+/* -------------------------------------------*/
+
