ce8eb08144f2f5b3f3af2c0fe64f08cfe567c85a
Amir Pourghaed <ampourgh@gmail.com>
2018-01-20 11:00:37 -0600

    chore: go through tutorial on proptypes

--- React/reactPractice.js
+++ React/reactPractice.js
@@ -947,6 +947,11 @@ export const GuineaPigs = (props) => {
   );
 }
 
+// propType defined
+GuineaPigs.propTypes = {
+  src: React.PropTypes.string.isRequired
+};
+
 // ./containers/GuineaPigsContainer.js
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -1017,5 +1022,71 @@ ReactDOM.render(
 	<Friend />,
 	document.getElementById('app')
 );
+
+/* -------------------------------------------*/
+
+// Tutorial on proptypes
+
+// BestSeller.js
+import React from 'react';
+
+export class BestSeller extends React.Component {
+  render() {
+    return (
+      <li>
+        Title: <span>
+          {this.props.title}
+        </span><br />
+        
+        Author: <span>
+          {this.props.author}
+        </span><br />
+        
+        Weeks: <span>
+          {this.props.weeksOnList}
+        </span>
+      </li>
+    );
+  }
+}
+
+BestSeller.propTypes = {
+  title: 				React.PropTypes.string.isRequired,
+  author:				React.PropTypes.string.isRequired,
+  weeksOnList:	React.PropTypes.number.isRequired
+};
+
+// BookList.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BestSeller } from './BestSeller';
+
+export class BookList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Best Sellers</h1>
+        <div>
+          <ol>
+            <BestSeller 
+              title="Glory and War Stuff for Dads" 
+              author="Sir Eldrich Van Hoorsgaard" 
+              weeksOnList={10} />
+            <BestSeller 
+              title="The Crime Criminals!" 
+              author="Brenda Sqrentun" 
+              weeksOnList={2} />
+            <BestSeller
+              title="Subprime Lending For Punk Rockers" 
+              author="Malcolm McLaren" 
+              weeksOnList={600} />
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<BookList />, document.getElementById('app'))
 
 /* -------------------------------------------*/
