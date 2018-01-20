cca1527479d139bc01a8cb690fa6a648c6b435d5
Amir Pourghaed <ampourgh@gmail.com>
2018-01-19 18:35:28 -0600

    chore: add sibling.js

--- React/reactPractice.js
+++ React/reactPractice.js
@@ -741,14 +741,49 @@ class Parent extends React.Component {
 ReactDOM.render(<Parent />, document.getElementById('app'));
 
 /* -------------------------------------------*/
+// PARENT
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Child } from './Child';
+import { Sibling } from './Sibling';
 
-// Child 
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { name: 'Frarthur' };
+
+    this.changeName = this.changeName.bind(this);
+  }
+
+  changeName(newName) {
+    this.setState({
+      name: newName
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Child onChange={this.changeName} />
+        <Sibling name={this.state.name} />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Parent />,
+  document.getElementById('app')
+);
+
+// CHILD.JS
 import React from 'react';
 
 export class Child extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -760,54 +795,32 @@ export class Child extends React.Component {
   render() {
     return (
       <div>
-        <h1>
-          Hey my name is {this.props.name}!
-        </h1>
-        <select id="great-names" onChange={this.handleChange}>
-          <option value="Frarthur">
-            Frarthur
-          </option>
+        <select
+          id="great-names"
+          onChange={this.handleChange}>
 
-          <option value="Gromulus">
-            Gromulus
-          </option>
-
-          <option value="Thinkpiece">
-            Thinkpiece
-          </option>
+          <option value="Frarthur">Frarthur</option>
+          <option value="Gromulus">Gromulus</option>
+          <option value="Thinkpiece">Thinkpiece</option>
         </select>
       </div>
     );
   }
 }
 
-// Parent 
-
+// SIBLING.JS
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Child } from './Child';
 
-class Parent extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { name: 'Frarthur' };
-    
-    this.changeName = this.changeName.bind(this);
-  }
-  
-  changeName(newName) {
-    this.setState({
-      name: newName
-    });
-  }
-
+export class Sibling extends React.Component {
   render() {
-    return <Child name={this.state.name} onChange={this.changeName} />
+    const name = this.props.name;
+    return (
+      <div>
+        <h1>Hey, my name is {name}!</h1>
+        <h2>Don't you think {name} is the prettiest name ever?</h2>
+        <h2>Sure am glad that my parents picked {name}!</h2>
+      </div>
+    );
   }
 }
-
-ReactDOM.render(
-	<Parent />,
-	document.getElementById('app')
-);
+/* -------------------------------------------*/
