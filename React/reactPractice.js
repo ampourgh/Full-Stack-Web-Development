444474188a778bfa434faa91b0a0a9335c7d5383
Amir Pourghaed <ampourgh@gmail.com>
2018-01-20 11:24:27 -0600

    chore: user input form

--- React/reactPractice.js
+++ React/reactPractice.js
@@ -1090,3 +1090,39 @@ export class BookList extends React.Component {
 ReactDOM.render(<BookList />, document.getElementById('app'))
 
 /* -------------------------------------------*/
+
+// user input form 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class Input extends React.Component {
+  constructor(props) {
+		super(props);
+
+		this.state = { userInput: '' };
+
+		this.handleUserInput = this.handleUserInput.bind(this);
+	}
+  handleUserInput(e) {
+    this.setState({
+	    userInput: e.target.value
+	  });
+  }
+  
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.handleUserInput} value={this.state.userInput}/>
+        <h1>{this.state.userInput}</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+	<Input />,
+	document.getElementById('app')
+);
+
+/* -------------------------------------------*/
