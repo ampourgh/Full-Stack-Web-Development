7152dbc66f7f8f526d6b31013cb4dda474780529
Amir Pourghaed <ampourgh@gmail.com>
2018-01-22 19:43:47 -0600

    feat: search bar now has a state
    The input can change depending on the user's input.

--- YouTube_React/src/components/search_bar.js
+++ YouTube_React/src/components/search_bar.js
@@ -1,7 +1,30 @@
-import React from 'react';
+// syntactic equivalent of const Component = React.Component
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input/>; // a call is being made here of React.createElement
-};
+// class component with all functionality React component has
+class SearchBar extends Component {
+
+  // state
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  // render function, must contain some JSX
+  render() {
+
+    // observe change in input with onChange
+    return (
+      <div>
+        <input
+          // controlled component
+          value = {this.state.term}
+          onChange={ event => this.setState({ term: event.target.value }) } />
+        Value of the input: {this.state.term}
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
