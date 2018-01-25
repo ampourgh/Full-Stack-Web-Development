1e58f82ce292870bb44b3da4f7224012538df59a
Amir Pourghaed <ampourgh@gmail.com>
2018-01-24 20:13:09 -0600

    bug: remove search near search bar

--- YouTube_React/src/components/search_bar.js
+++ YouTube_React/src/components/search_bar.js
@@ -16,12 +16,12 @@ class SearchBar extends Component {
 
     // observe change in input with onChange
     return (
-      <div>
+      <div className="search-bar">
         <input
           // controlled component
           value = {this.state.term}
           onChange={ event => this.setState({ term: event.target.value }) } />
-        Value of the input: {this.state.term}
+        
       </div>
     );
   }
