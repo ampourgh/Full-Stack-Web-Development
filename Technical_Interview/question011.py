01b9e52a9390ad594b3e0c44c7534200372400fc
Amir Pourghaed <ampourgh@gmail.com>
2018-01-27 11:32:54 -0600

    Create question011.py

---
+++ Technical_Interview/question011.py
@@ -0,0 +1,33 @@
+"""
+Print a single line of  space-separated integers denoting the final state of the array after performing d left rotations.
+
+Sample Input
+
+5 4
+1 2 3 4 5
+
+Sample Output
+
+5 1 2 3 4
+"""
+
+#!/bin/python3
+
+import sys
+
+def leftRotation(a, d):
+    
+    #go through range of d
+    for x in range(0, d):
+        value = a[0]
+        del a[0]
+        a.append(value)
+     
+    return a
+
+if __name__ == "__main__":
+    n, d = input().strip().split(' ')
+    n, d = [int(n), int(d)]
+    a = list(map(int, input().strip().split(' ')))
+    result = leftRotation(a, d)
+    print (" ".join(map(str, result)))
