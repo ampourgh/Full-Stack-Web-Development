af56c1129833692f3c08ab21184054e367dd3557
Amir Pourghaed <ampourgh@gmail.com>
2018-01-29 21:24:12 -0600

    chore: explanation of the steps taken

--- Technical_Interview/question012.py
+++ Technical_Interview/question012.py
@@ -1,3 +1,15 @@
+"""
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+"""
+
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
@@ -5,12 +17,15 @@ class ListNode(object):
         self.next = None
 
 class Solution(object):
+    # function takes in 2 pre-created linked-lists
     def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
+        
+        # empty strings and function used for adding the values together
         l2_value = ""
         l1_value = ""
         
@@ -21,17 +36,12 @@ class Solution(object):
             if l2 != None:
                 l2_value += str(l2.val)
                 l2 = l2.next
-            
-        l1_value = int(l1_value[::-1])
-        l2_value = int(l2_value[::-1])
         
-        print(l1_value)
-        print(l2_value)
-
-        l_sum = l1_value + l2_value
+        # reverse values, make them integers, and add them together  
+        l_sum = int(l1_value[::-1]) + int(l2_value[::-1])
         
+        # reverse sum back around and split the integers into separate integers
         l_sum = str(l_sum)[::-1]
-        
         l_sum = [int(x) for x in l_sum]
 
         return l_sum
