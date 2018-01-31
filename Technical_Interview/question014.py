515b9754dbc36e72c0669f603dd5f95c2e8a09c9
Amir Pourghaed <ampourgh@gmail.com>
2018-01-31 00:01:43 -0600

    bug: turn numbers into float so decimal is shown

--- Technical_Interview/question014.py
+++ Technical_Interview/question014.py
@@ -1,10 +1,23 @@
+"""
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+"""
+
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
+
         nums = []
         
         nums.extend(nums1)
@@ -14,7 +27,7 @@ class Solution(object):
         
         while len(nums) != 1:
             if len(nums) == 2:
-                val = (nums[0] + nums[1])/2
+                val = (float(nums[0]) + float(nums[1]))/2
                 del nums[1]
                 del nums[0]
                 
