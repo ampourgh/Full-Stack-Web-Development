ce808bdbf5eb2638d18d198949950687f49c7fee
Amir Pourghaed <ampourgh@gmail.com>
2018-01-30 22:25:57 -0600

    feat: search for sequence of unique values

--- Technical_Interview/question013.py
+++ Technical_Interview/question013.py
@@ -1,3 +1,4 @@
+# look for any unique value in a string
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
@@ -15,3 +16,50 @@ class Solution(object):
                 unique_letters.append(alpha)
         
         return len(unique_letters)
+    
+# look for sequence of unique values in a string
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        unique_letters = []
+        unique_l = []
+        previous_longest = []
+        
+        s = list(s)
+        
+        for alpha in s:
+            
+            if len(unique_letters) == 0:
+                print('new alpha')
+                print(alpha)
+                unique_letters.append(alpha)
+                unique_l.append(alpha)
+            else:
+                count = 0
+                if unique_letters != unique_l:
+                    unique_letters = unique_l
+                for letter in unique_letters:
+                    print('unique_letters:')
+                    print(unique_l)
+                    print(unique_letters)
+                    
+                    count += 1
+                    if alpha == letter:
+                        print('same')
+                        print(alpha)
+                        print(letter)
+                        if len(unique_l) > len(previous_longest):
+                            previous_longest = unique_l
+                        unique_l = []
+                        unique_letters = []
+                        unique_letters.append(alpha)
+                        unique_l.append(alpha)
+                    elif count == len(unique_letters) and alpha != letter:
+                        print('unique')
+                        print(alpha)
+                        unique_l.append(alpha)
+        
+        return len(previous_longest)
