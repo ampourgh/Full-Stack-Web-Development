023ffbcce89951cb7dc246e6c7afbec76e7b921b
Amir Pourghaed <ampourgh@gmail.com>
2018-03-08 21:23:34 -0600

    Update JavaScript.js

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -160,3 +160,56 @@ while (currentCard !== 'Spade') {
 console.log('You found a spade!');
 
 /* ---------------------------------- */
+
+let artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+
+artists.forEach(function(artist) {
+  console.log(artist + ' is one of my favorite artists.');
+});
+
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+
+fruits.forEach(fruit => console.log('I want to eat a ' + fruit));
+
+/* ---------------------------------- */
+
+let numbers = [1, 2, 3, 4, 5];
+
+let squareNumbers = numbers.map(function(number) {
+  return number * number;
+});
+
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+let secretMessage = animals.map(animal => animal[0]);
+
+/* ---------------------------------- */
+
+console.log(squareNumbers);
+
+let things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+let onlyNumbers = things.filter(function(thing) {
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
+
+let randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+let smallNumbers = randomNumbers.filter(number => number < 250);
+
+console.log(smallNumbers);
+
+let favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+let longFavoriteWords = favoriteWords.filter(word => word.length > 7);
+
+// Refactor the code above using arrow function syntax
+console.log(longFavoriteWords);
+
+/* ---------------------------------- */
