263ab36efb73de1c04703105bdea87a1c9bc8bf6
Amir Pourghaed <ampourgh@gmail.com>
2018-04-08 17:00:37 -0500

    Add files via upload

---
+++ promotion/coupons/js/form.js
@@ -0,0 +1,63 @@
+new Vue({
+  el: '#text-insert',
+  data: {
+    name: '',
+    promotion: 'Special offer at restaurant for a buy 1 get another for insert text 50% off.'
+  }
+});
+
+new Vue({
+  el: '#logo-upload',
+  data: {
+    image: ''
+  },
+  methods: {
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createImage(files[0]);
+    },
+    createImage(file) {
+      var image = new Image();
+      var reader = new FileReader();
+      var vm = this;
+
+      reader.onload = (e) => {
+        vm.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    removeImage: function (e) {
+      this.image = '';
+    }
+  }
+})
+
+new Vue({
+  el: '#image-upload',
+  data: {
+    image: ''
+  },
+  methods: {
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createImage(files[0]);
+    },
+    createImage(file) {
+      var image = new Image();
+      var reader = new FileReader();
+      var vm = this;
+
+      reader.onload = (e) => {
+        vm.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    removeImage: function (e) {
+      this.image = '';
+    }
+  }
+})
---
+++ promotion/coupons/js/toggle.js
@@ -0,0 +1,12 @@
+// toggle form so user can see preview of the promotion
+$(document).ready(function(){
+    $("#toggle").click(function(){
+        $("#hide0").toggle();
+        $("#hide1").toggle();
+        $("#hide2").toggle();
+        $("#hide3").toggle();
+        $("#hide4").toggle();
+        $("#hide5").toggle();
+        $("#hide6").toggle();
+    });
+});
