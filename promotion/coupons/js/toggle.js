a0f518046932302fa475939cc73abe0fa9619927
Amir Pourghaed <ampourgh@gmail.com>
2018-04-10 13:43:56 -0600

    feat: change to php doc and additional toggle

--- promotion/coupons/js/toggle.js
+++ promotion/coupons/js/toggle.js
@@ -1,12 +1,18 @@
-// toggle form so user can see preview of the promotion
+// CTRL + SHIFT + DEL -> Clear browsing data -- Need to do in order for JQuery to work in PHP
+
 $(document).ready(function(){
     $("#toggle").click(function(){
-        $("#hide0").toggle();
-        $("#hide1").toggle();
-        $("#hide2").toggle();
-        $("#hide3").toggle();
-        $("#hide4").toggle();
-        $("#hide5").toggle();
-        $("#hide6").toggle();
+
+      // toggle between Edit and preview
+      $(this).text() === 'Edit' ? $(this).text('Preview') : $(this).text('Edit');
+
+      // toggle form so user can see preview of the promotion
+      $("#hide0").toggle();
+      $("#hide1").toggle();
+      $("#hide2").toggle();
+      $("#hide3").toggle();
+      $("#hide4").toggle();
+      $("#hide5").toggle();
+      $("#hide6").toggle();
     });
 });
