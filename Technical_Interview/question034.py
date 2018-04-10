ae25b5463e634cc078e2ab25a756298cb0cbbfe9
Amir Pourghaed <ampourgh@gmail.com>
2018-04-09 22:18:40 -0600

    refactor: come up with alternative method

--- Technical_Interview/question034.py
+++ Technical_Interview/question034.py
@@ -28,3 +28,21 @@ goodbye = 'I am going to bed now, goodnight.'
 uniqueLetterCount(hello)
 uniqueLetterCount(elipses)
 uniqueLetterCount(goodbye)
+
+# Alternative method
+
+def uniqueLetterCount2(string):
+    array = [s for s in string.strip() if s.isalpha() and s != ' ']
+    final = ['0']
+    for i in array:
+        if not any(i in item for item in final):
+            final.append(i)
+    print(len(final[1:]))
+
+hello = 'Hello world.'
+elipses = '.......'
+goodbye = 'I am going to bed now, goodnight.'
+
+uniqueLetterCount2(hello)
+uniqueLetterCount2(elipses)
+uniqueLetterCount2(goodbye)
