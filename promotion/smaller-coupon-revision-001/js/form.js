a584c5df0b6d300287a76f967351fa29422b50a4
Amir Pourghaed <ampourgh@gmail.com>
2018-04-14 15:22:14 -0500

    Add files via upload

---
+++ promotion/smaller-coupon-revision-001/js/form.js
@@ -0,0 +1,46 @@
+new Vue({
+  el: '#app',
+  data: {
+    items: [
+       {
+         image: false,
+       }
+    ],
+    form: {
+      formId: '1',
+      title: 'Grocery Item',
+      description: 'Grocery item information goes here, or not. You can leave this section blank if you want to!',
+      button: '',
+      reducedPrice: '7.49/lb',
+      originalPrice: 14.99,
+      percentageOff: 0
+    },
+    showSubmitFeedback: false
+  },
+  methods: {
+    onFileChange(item, e) {
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+      this.createImage(item, files[0]);
+    },
+    createImage(item, file) {
+      var image = new Image();
+      var reader = new FileReader();
+
+      reader.onload = (e) => {
+        item.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    removeImage: function (item) {
+      item.image = false;
+    },
+    fakeSubmit(){
+      this.showSubmitFeedback = true;
+      setTimeout(() => {
+        this.showSubmitFeedback = false;
+      }, 1500);
+    }
+  }
+});
