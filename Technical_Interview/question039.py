c4e5ddbab0dbc253b93eb75df1f2f3cbef6a69eb
Amir Pourghaed <ampourgh@gmail.com>
2018-04-30 20:25:47 -0500

    Update question039.py

--- Technical_Interview/question039.py
+++ Technical_Interview/question039.py
@@ -53,3 +53,46 @@ def merge_the_tools(string, k):
             grp2 += i[0]            
         print(grp2)
         grp2 = ''
+
+# Correct method - O(n*n):
+# poor efficiency, low space necessity
+
+def merge_the_tools(s, n):  
+  
+  # [iter(s)]*n -- iterates the string n times 
+  # an iterator is an object that is used traverse a container, 
+  # particularly lists. 
+  # EX: [[iter(s)] * 3] = ([iter(s), iter(s), iter(s)])
+  
+  iter_string = [iter(s)] * n
+  
+  for part in zip(*iter_string):
+    
+    # since all the iterators are the same, 
+    # they are divided into 3
+    # EX: 
+    # ('A', 'A', 'B')
+    # ('d', 'e', 'f')
+    # ('g', 'h', 'i')
+      
+    d = dict()
+    
+    # search dict() for each value in divided string,
+    # if item not found, return value into list
+    uniques = [ d.setdefault(c, c) for c in part if c not in d ]
+    print(''.join(uniques))
+      
+merge_the_tools('AABCAAADA', 3)
+
+
+# user version
+print('\nUser version:')
+for section in zip(*[iter('123456789')]*3):
+  print(''.join(section))
+  
+# expanded
+print('\nexpanded version:')
+s = 'abcdefghi'
+it = iter(s)
+for part in zip(it, it, it):
+    print(''.join(part))
