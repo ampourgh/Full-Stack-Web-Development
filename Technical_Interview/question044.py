ed2750fce44eb5babc1b36f19d8f280571c707cd
Amir Pourghaed <ampourgh@gmail.com>
2018-05-19 22:03:56 -0500

    feat: print arrays individually in desc order

--- Technical_Interview/question044.py
+++ Technical_Interview/question044.py
@@ -1,13 +1,12 @@
 def add(array):
+
   diction = dict()
   array = sorted(array)
   prev_value = None
   indexu = 1
-  narray = []
+
   for i in range(len(array)):
-    print(array[i])
-    print(prev_value)
-    print(indexu)
+
     if array[i] == prev_value:
       indexu += 1
       prev_value = array[i]
@@ -20,22 +19,36 @@ def add(array):
         indexu = 1
         
     else:
-      prev_value = array[i]
-      if array[i] == array[i + 1]:
-        pass
-      else:
-        diction[i] = [array[i], indexu]
-        indexu = 1
-      
+      if i + 1 != len(array):
+        prev_value = array[i]
+        if array[i] == array[i + 1]:
+          pass
+        else:
+          diction[i] = [array[i], indexu]
+          indexu = 1
   
   print(diction)
-  # array = []
-  # for index, value in enumerate(diction):
-  #   array.append(value)
+  array = []
+  for index, val1 in diction.items():
+    array.append(val1)
     
-  # print(array)
+  def getKey(item):
+    return item[1]
+    
+  array = sorted(array, key = getKey)
+  array = array[::-1]
+  
+  for setVals in array:
+    print("%s: %s" % (setVals[0], setVals[1]))
+    
 
-arr = ['UK', 'China', 'China', 'China', 'USA', 'USA', 'France', 'New Zealand', 'UK', 'France', 'USA', 'China', 'USA', 'USA'] 
+arr = ['NA', 'NA', 'NA', 'EU', 'KO', 'CH', 'CH', 'CH', 'CH', 'KO', 'KO', 'KO', 'KO', 'KO', 'KO', 'KO', 'KO', 'Wildcard'] 
 add(arr)
 
-# >>> {3: ['China', 4], 5: ['France', 2], 6: ['New Zealand', 1], 8: ['UK', 2], 13: ['USA', 5]}
+"""
+>>> {3: ['CH', 4], 4: ['EU', 1], 13: ['KO', 9], 16: ['NA', 3]}
+KO: 9
+CH: 4
+NA: 3
+EU: 1
+"""
