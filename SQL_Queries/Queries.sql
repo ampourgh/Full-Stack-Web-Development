556f7fc792b49ae858a9166ba7c161e9c035a9b5
Amir Pourghaed <ampourgh@gmail.com>
2018-05-21 23:05:08 -0500

    Update and rename Queries.mdf to Queries.sql

---
+++ SQL_Queries/Queries.sql
@@ -0,0 +1,23 @@
+\* 
+Query the names of all the Japanese cities in the CITY table. 
+The COUNTRYCODE for Japan is JPN. 
+https://s3.amazonaws.com/hr-challenge-images/8137/1449729804-f21d187d0f-CITY.jpg
+*\
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN';
+
+// All attributes
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN;
+
+\*
+Query the names of all American cities in CITY with populations larger than 120000. 
+The CountryCode for America is USA.
+*\
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000;
