23312d5968f4d88c187b7c46b6556217c2504b10
Amir Pourghaed <ampourgh@gmail.com>
2018-05-30 22:04:06 -0500

    Update question048.py

--- Technical_Interview/question048.py
+++ Technical_Interview/question048.py
@@ -7,15 +7,25 @@ N = [(5, 'Harry'), (37.21, 'Berry'), (37.21, 'Tina'), (37.2, 'Akriti'), (41, 'Ha
 
 students = []
 
+def getKey(item):
+  return item[1]
+  
 for i in range(len(N)):
     grade = [N[i][1], float(N[i][0])]
     students.append(grade)    
-    
-second_highest = students[0][1]
+
+students = sorted(students, key = getKey)[::-1]
+print(students)
+
+second_highest = students[1][1]
+second_arr = [students[1][0], students[1][1]]
 
 for name, grade in students:
-    if grade > second_highest:
-        second_highest = grade
-        break
+    if grade == second_highest:
+        second_arr.append([name, grade])
 
 print('\n'.join([name for name, grade in sorted(students) if grade == second_highest]))
+
+>>> [['Harsh', 41.0], ['Tina', 37.21], ['Berry', 37.21], ['Akriti', 37.2], ['Harry', 5.0]]
+>>> Berry
+>>> Tina
