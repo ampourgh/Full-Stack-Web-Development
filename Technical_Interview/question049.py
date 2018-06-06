4b48997e0e3c4265cc15df9d739b7b0609f0abda
Amir Pourghaed <ampourgh@gmail.com>
2018-06-05 20:27:07 -0600

    Update question049.py

--- Technical_Interview/question049.py
+++ Technical_Interview/question049.py
@@ -11,3 +11,20 @@ cols = rint < rfloat and rint + 1 or rint
 
 for i in range(cols):
     print(s[i::cols], end=' ')
+   
+# Alternative Method
+
+s = list('haveanicelife'.strip())
+L =  len(s)
+row = math.floor(L**(1/2))
+col = math.ceil(L**(1/2))
+
+for i in range(col):
+    x = 0     
+    while True:
+        try:
+            print(s[i+x],end='')
+            x+= col
+        except:
+            break        
+    print(' ',end='')
