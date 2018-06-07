7501979ff62d29c5fda1a8bce5fb8d1134a08b14
Amir Pourghaed <ampourgh@gmail.com>
2018-06-06 22:40:30 -0600

    Update question050.py

--- Technical_Interview/question050.py
+++ Technical_Interview/question050.py
@@ -2,26 +2,38 @@
 Capitalize the start of a sentence
 """
 
-
-s = 'hello world my, name is x. what is your name? ha! ha! ha!'
-capit = False
-first = True
+s = 'hell broke lose. hello world my, name is x. what is your name? ha! ha! ha!'
+capit = 0
+first = 1
 sentenceEnd = ['.', '?', '!']
+contraction = ['re', 've', 'll']
+contraction2 =  ['s', 't']
+
 for x in s[:].split():
   
-  if capit == True:
+  if capit == 1:
     s = s.replace(x, x.capitalize())
-    capit = False
+    capit = 0
 
-  if first == True:
+  if first == 1:
     s = s.replace(x, x.capitalize())
-    first = False
+    first = 1
+  elif any(x[-1:] == eachEnd for eachEnd in sentenceEnd):
+    capit = 1
+  
+  # contraction needs work
+  for i in contraction: 
+    if i == x[-2:]:
+      x = list(x)
+      x.insert(-2, "\'")
+      x = "".join(str(alpha) for alpha in x)
+      print(x)
+    
+  
   """
   elif (x[-1:] == '.' 
         or x[-1:] == '?' 
         or x[-1:] == '!'):
   """
-  elif any(x[-1:] == eachEnd for eachEnd in sentenceEnd):
-    capit = True
 
 print(s)
