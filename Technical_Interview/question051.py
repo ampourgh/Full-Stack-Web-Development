3926309bb3c5af1372741d93df7fbf011b258b48
Amir Pourghaed <ampourgh@gmail.com>
2018-06-08 00:04:56 -0500

    Update question051.py

--- Technical_Interview/question051.py
+++ Technical_Interview/question051.py
@@ -1,10 +1,33 @@
 n2 = ['0 ab', '0 ef', '0 ab', '0 ef', '0 ij', '0 to', '1 be','1 or', '2 not', '2 to', '3 be', '4 ij', '4 that', '4 is', '4 the', '5 question', '6 cd', '6 gh', '6 cd', '6 gh']
 
-n3 = [['0', 'ab'], ['0', 'ef'], ['0', 'ab'], ['0', 'ef'], ['0', 'ij'], ['0', 'to'], ['1', 'be'],['1', 'or'], ['2', 'not'], ['2', 'to'], ['3', 'be'], ['4', 'ij'], ['4', 'that'], ['4', 'is'], ['4', 'the'], ['5', 'question'], ['6', 'cd'], ['6', 'gh'], ['6', 'cd'], ['6', 'gh']]
+n3 = [
+        ['6', 'cd'],
+        ['0', 'ef'],
+        ['6', 'gh'],
+        ['4', 'ij'],
+        ['0', 'ab'],
+        ['6', 'cd'],
+        ['0', 'ef'],
+        ['6', 'gh'],
+        ['0', 'ij'],
+        ['4', 'that'],
+        ['3', 'be'],
+        ['0', 'to'],
+        ['1', 'be'],
+        ['5', 'question'],
+        ['1', 'or'],
+        ['2', 'not'],
+        ['4', 'is'],
+        ['2', 'to'],
+        ['4', 'the']
+      ]
 
 n1 = '0 ab 0 ef 0 ab 0 ef 0 ij 0 to 1 be 1 or 2 not 2 to 3 be 4 ij 4 that 4 is 4 the 5 question 6 cd 6 gh 6 cd 6 gh'
 
-n = n3
+def getKey(item):
+  return item[0]
+
+n = sorted(n3, key = getKey)
 
 num_dict = {}
 maxNum = 0
