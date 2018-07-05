9d9e388dd6c9cc922871c9be48b82b8e23fbf346
Amir Pourghaed <ampourgh@gmail.com>
2018-07-04 23:16:23 -0600

    Update question055.py

--- Technical_Interview/question055.py
+++ Technical_Interview/question055.py
@@ -2,14 +2,21 @@
 
 listVal = [4, 3, 2, 1, 3, 4]
 def checkCube(list_val): 
-  count = 1
+  count = 0
+  list_val_inverse = list_val[::-1]
   for number in list_val:
-      inverse = count * -1 
-      if (list_val[count] + 1) == list_val[::inverse] or list_val[count] == (list_val[::inverse] + 1):
+      print(list_val[count])
+      print(list_val_inverse[count])
+
+      if (list_val[count] == list_val_inverse[count] 
+          or (list_val[count] + 1) == list_val_inverse[count] 
+          or (list_val[count] - 1) == list_val_inverse[count]):
+
           count += 1
-      if (count >= (len(listVal) / 2)):
-        return True
       else:
         return False
+        
+      if (count >= (len(list_val) / 2)):
+        return True
       
-checkCube(listVal)
+print(checkCube(listVal))
