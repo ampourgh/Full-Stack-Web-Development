7b5e36f3fefe3e98cfda82c814bb4e2e3f23e03a
Amir Pourghaed <ampourgh@gmail.com>
2018-07-04 23:06:54 -0600

    Update question055.py

--- Technical_Interview/question055.py
+++ Technical_Interview/question055.py
@@ -1,13 +1,15 @@
 # syntax goes from larger to smaller from either side
 
-list = [4, 3, 2, 1, 3, 4]
-
-count = 1
-for number in list:
-    inverse = count * -1 
-    if (list[count] + 1) == list[::inverse] or list[count] == (list[::inverse] + 1):
-        pass
-    else:
-        return false
-return true
-    
+listVal = [4, 3, 2, 1, 3, 4]
+def checkCube(list_val): 
+  count = 1
+  for number in list_val:
+      inverse = count * -1 
+      if (list_val[count] + 1) == list_val[::inverse] or list_val[count] == (list_val[::inverse] + 1):
+          count += 1
+      if (count >= (len(listVal) / 2)):
+        return True
+      else:
+        return False
+      
+checkCube(listVal)
