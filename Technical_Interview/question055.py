92fa5266bc7f4caeaaa7c33c0b63c87d8a4c0eab
Amir Pourghaed <ampourgh@gmail.com>
2018-07-04 22:59:22 -0600

    Update question055.py

--- Technical_Interview/question055.py
+++ Technical_Interview/question055.py
@@ -1,5 +1,13 @@
-// syntax goes from larger to smaller from either side
+# syntax goes from larger to smaller from either side
 
-t = [1, 2, 3, 4, 5]
-for i in t:
-    print(input())
+list = [4, 3, 2, 1, 3, 4]
+
+count = 1
+for number in list:
+    inverse = count * -1 
+    if (list[count] + 1) == list[::inverse] or list[count] == (list[::inverse] + 1):
+        pass
+    else:
+        return false
+return true
+    
