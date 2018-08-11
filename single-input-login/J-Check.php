80d098a8e31db8af2fcd29df225efaa4e6cc6f9f
ampourgh <ampourgh@gmail.com>
2018-08-10 22:28:48 -0600

    reorganize repo

--- single-input-login/J-Check.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-include 'J-Init.php';
-session_start();
-  function get_results($username_post) {
-
-    $result = $mysqli->query("SELECT * FROM accounts WHERE username='$username_post'");
-    return $result;
-  }
-?>
