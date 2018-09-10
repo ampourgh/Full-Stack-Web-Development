819ea47ed2283568c9b654fb62f32485736edc78
Emerson Pourghaed <ampourgh@gmail.com>
2018-09-09 23:29:48 -0500

    bug fix

--- Technical-Interview/question056.py
+++ Technical-Interview/question056.py
@@ -1,18 +1,25 @@
-m, n = '3 2'.split(' ')
+inputArr = [
+            '3 2',
+            '1 5 3',
+            '3 1',
+            '5 7'
+]
+
+m, n = inputArr[0].split(' ')
 m, n = int(n), int(m)
 
 print(n,' ',m)
 
-arr = [int(i) for i in '1 5 3'.split(' ') ]
+arr = [int(i) for i in inputArr[1].split(' ') ]
 
 print(arr)
 
-happinessMeter = 0
+happy = 0
 
-like = set([int(j) for j in '3 1'.split(' ') ])
-dislike = set([int(j) for j in '5 7'.split(' ') ])
+like = set([int(j) for j in inputArr[2].split(' ') ])
+dislike = set([int(j) for j in inputArr[3].split(' ') ])
 
 for i in arr:
-  happinessMeter += 1 if i in like else happinessMeter -= 1
+  happy = happy + 1 if i in like else happy - 1
 
-print('{}'.format(happinessMeter))
+print('{}'.format(happy))
