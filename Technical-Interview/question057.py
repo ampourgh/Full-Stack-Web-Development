ea8cf663e307770ba8a6217be0c9992d145121f5
Emerson Pourghaed <ampourgh@gmail.com>
2018-09-30 13:32:21 -0500

    bug: fix question 057
    Fix the question to work on compiler.

--- Technical-Interview/question057.py
+++ Technical-Interview/question057.py
@@ -1,15 +1,27 @@
-from itertools import product
-
 inputArr = [
   '3, 1000',
-  '2, 5, 4'
+  '2, 5, 4',
   '3, 7, 8, 9',
   '5, 5, 7, 8, 9, 10'
 ]
 
 K, M = map(int, inputArr[0].split(', '))
-N = (list(map(int, inputArr[0].split(', ')))[1:] for _ in range(K))
 
-results = map(lambda x: sum(i**2 for i in x)%M, product(*N))
+print(K)
+print(M)
 
-print(max(results))
+arrayN = []
+
+for _i_ in range(K + 1):
+  if _i_ != 0:
+    arrayN.append([int(x) for x in inputArr[_i_].split(', ')])
+
+print(arrayN)
+
+from itertools import product
+possible_combination = list(product(*arrayN))
+
+def func(nums):
+    return sum(x*x for x in nums) % M
+
+print(max(list(map(func, possible_combination))))
