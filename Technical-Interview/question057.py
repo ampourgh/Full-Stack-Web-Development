db3754b67330359c8320682468bce92249d6c1df
Emerson Pourghaed <ampourgh@gmail.com>
2018-09-30 14:13:48 -0500

    refactor: easier to read code

--- Technical-Interview/question057.py
+++ Technical-Interview/question057.py
@@ -1,27 +1,37 @@
+from sys import exit
+from itertools import product
+
 inputArr = [
-  '3, 1000',
-  '2, 5, 4',
-  '3, 7, 8, 9',
-  '5, 5, 7, 8, 9, 10'
+    '3, 1000',
+    '2, 5, 4',
+    '3, 7, 8, 9',
+    '5, 5, 7, 8, 9, 10'
 ]
 
+# 1 <= K <= 7
+# 1 <= M <= 1000
 K, M = map(int, inputArr[0].split(', '))
 
-print(K)
-print(M)
+if (K > 7 or K < 1):
+    exit("K is bigger than parameters, the integor needs to be between 1 and 7.")
+
+if (M > 1000 or M < 1):
+    exit("M is bigger than parameters, the integor needs to be between 1 and 1,000.")
+
+# print('K: ',K, ' M: ', M)
 
 arrayN = []
 
-for _i_ in range(K + 1):
-  if _i_ != 0:
+for _i_ in range(1, K + 1):
     arrayN.append([int(x) for x in inputArr[_i_].split(', ')])
 
 print(arrayN)
 
-from itertools import product
-possible_combination = list(product(*arrayN))
-
-def func(nums):
+def squaredSums(nums):
     return sum(x*x for x in nums) % M
 
-print(max(list(map(func, possible_combination))))
+possible_combinations = list(product(*arrayN))
+
+possible_sums = list(map(squaredSums, possible_combinations))
+
+print(max(possible_sums))
