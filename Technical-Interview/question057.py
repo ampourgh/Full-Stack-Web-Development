c14d3b7f85cdbb6e33dc9c0b8e715fe3b6668d62
Emerson Pourghaed <ampourgh@gmail.com>
2018-09-30 15:20:07 -0500

    doc: comment code

--- Technical-Interview/question057.py
+++ Technical-Interview/question057.py
@@ -8,10 +8,11 @@ inputArr = [
     '5, 5, 7, 8, 9, 10'
 ]
 
-# 1 <= K <= 7
-# 1 <= M <= 1000
 K, M = map(int, inputArr[0].split(', '))
 
+# exit if the int is larger than the parameters
+# 1 <= K <= 7
+# 1 <= M <= 1000
 if (K > 7 or K < 1):
     exit("K is bigger than parameters, the integor needs to be between 1 and 7.")
 
@@ -22,16 +23,22 @@ if (M > 1000 or M < 1):
 
 arrayN = []
 
-for _i_ in range(1, K + 1):
-    arrayN.append([int(x) for x in inputArr[_i_].split(', ')])
+# append to arrayN an array of the string items between inputArr's [1:]
+for i in range(1, K + 1):
+    arrayN.append([int(x) for x in inputArr[i].split(', ')])
 
-print(arrayN)
+# print(arrayN)
 
+# function: sum of square of possible combinations, modulo if greater than 1000
 def squaredSums(nums):
-    return sum(x*x for x in nums) % M
+    return sum(x**2 for x in nums) % M
 
+# put combinations of ints in own array
 possible_combinations = list(product(*arrayN))
 
+# get squared sum of possible combinations through squaredSum()
 possible_sums = list(map(squaredSums, possible_combinations))
 
-print(max(possible_sums))
+# print the max value
+print("""The sum of the maximum values from each arrays,
+         after being squared, is {}.""".format(max(possible_sums)))
