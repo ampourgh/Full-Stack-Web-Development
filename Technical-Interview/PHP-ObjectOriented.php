812df4b767055e11cbec0d1ba8857a4a7519a04f
Emerson Pourghaed <ampourgh@gmail.com>
2018-10-25 22:35:21 -0500

    php technical questions

--- Technical-Interview/PHP-ObjectOriented.php
+++ Technical-Interview/PHP-ObjectOriented.php
@@ -29,20 +29,19 @@
       $this->id = rand(100, 1000000000);
       $this->birth_name = 'Android' . $this->id;
       $this->operating_system = 'FreeWareOS';
-      $this->start_up_sound = 'Boop.';
+      $this->start_up_sound = 'boop';
 
       echo $this->id . " has been assigned.\n";
-
       Android::$population++;
 
     }
 
     public function __destruct() {
-      echo $this->birth_name . " is being destroyed.";
+      echo $this->birth_name . " is being destroyed.\n";
     }
 
     function __get($variable) {
-      echo "Ask for " . str_replace("_", " ", $variable) . "\n";
+      echo "Ask for " . str_replace("_", " ", $variable) . ".\n";
       return $this->$variable;
     }
 
@@ -59,30 +58,42 @@
           $this->start_up_sound = $value;
           break;
         default:
-          echo $variable . " not found.";
+          echo $variable . " not found.\n";
       }
-
-      echo "Set " . str_replace("_", " ", $variable) . " to " . $value . "\n";
+      echo "Set " . str_replace("_", " ", $variable) . " to " . $value . ".\n";
     }
 
     function run() {
-      echo $this->birth_name . " runs\n";
+      echo $this->birth_name . " operates economically.\n";
     }
   }
 
-  class Dog extends Android {
+  class Android_Gen_2 extends Android {
     function run() {
-      echo $this->birth_name . " runs like crazy\n";
+      echo $this->birth_name . " operates significantly fast.\n";
     }
   }
 
-  $android_clone = new Android();
+  class Android_Gen_3 extends Android_Gen_2 {
+    protected $telepathy;
 
-  echo $android_clone->birth_name . " says " . $android_clone->start_up_sound . "\n";
+    $telepathy = ' emits messages to you.';
+
+    function run() {
+      echo $this->birth_name . " has visions of the future.\n";
+    }
+  }
+
+  $android_clone = new Android_Gen_3();
+
+  echo $android_clone->birth_name . " says " . $android_clone->start_up_sound . ".\n";
+
+  $android_clone->telepathy . ".\n";
 
   $android_clone->birth_name = "l33t";
   $android_clone->operating_system = "Windows 2057";
   $android_clone->start_up_sound = "Beeonnn";
 
-  echo $android_clone->birth_name . " says " . $android_clone->start_up_sound;
+  echo $android_clone->birth_name . " says " . $android_clone->start_up_sound . ".\n";
+  $android_clone->run();
  ?>
--- Technical-Interview/PHP-technical.php
+++ Technical-Interview/PHP-technical.php
@@ -12,6 +12,7 @@ echo "<br />";
 echo $x;
 
 
+
 $str1 = 'powerful';
 $str2 = 'pow';
 if (strpos($str1,$str2)) { // RETURNS FALSE
@@ -22,14 +23,47 @@ if (strpos($str1,$str2)) { // RETURNS FALSE
 }
 
 
+
 $x = true;            // sets $x equal to true
 $x = true and false;  // results in false, but has no affect on anything
 var_dump($x);
+
+
 
 $pseudonym = 'Fake';
 $pseudonym[5] = 'RRR';
 // >>> Fake R
 strlen($pseudonym);
 // >>> 6
+
+
+
+$a = '1';
+$b = &$a;
+
+echo $b . "\n";
+//>>> 1
+
+$b = "2$b";
+
+echo $b;
+//>>> 21
+
+
+
+var_dump(0123 == 123); // bool(false) -- OCTAL instead of DECI
+var_dump('0123' == 123); // bool(true) -- COERCED into INT
+var_dump('0123' === 123); // bool(false) -- STRICT comparison
+
+
+
+$x = 3 + "15%" + "$25";
+// 3 + 15 + 0
+// starting character decides value
+echo $x;
+//>>> 18
+
+
+
 
  ?>
