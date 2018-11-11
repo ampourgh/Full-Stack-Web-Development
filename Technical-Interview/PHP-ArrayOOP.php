9514b510adc6d29cac868f93d8b21d762099cf75
Emerson Pourghaed <ampourgh@gmail.com>
2018-11-10 21:13:25 -0600

    add quicksort and change alphabatize() to sort()

--- Technical-Interview/PHP-ArrayOOP.php
+++ Technical-Interview/PHP-ArrayOOP.php
@@ -102,6 +102,60 @@ class Arr {
       echo "\n" . (microtime(TRUE)-$time). ' seconds';
     }
   }
+
+  public function _execute_quicksort($low, $high) {
+
+      if($low===null || $high===null){
+          return false;
+      }
+
+      if($low < $high) {
+          $pi = $this->_quicksort_partition($low, $high);
+          $this->_execute_quicksort($low, $pi - 1);
+          $this->_execute_quicksort($pi + 1, $high);
+      }
+  }
+
+  public function _quicksort_partition($low, $high) {
+
+      if($low===null || $high===null){
+          return false;
+      }
+
+      $pivot = $this->arr[$high];
+
+      $index = $low - 1;
+
+      for($incrementingVal = $low; $incrementingVal <= $high-1; $incrementingVal++) {
+          if($this->arr[$incrementingVal] <= $pivot) {
+              $index++;
+              $this->_quicksort_swap($index, $incrementingVal);
+          }
+      }
+
+      $this->_quicksort_swap($index+1,$high);
+
+      return $index + 1;
+  }
+
+  public function _quicksort_swap($index, $incrementingVal) {
+      $p=$this->arr[$index];
+      $q=$this->arr[$incrementingVal];
+      $this->arr[$index]=$q;
+      $this->arr[$incrementingVal]=$p;
+  }
+
+  public function _quicksort_check_budget($arr, $budget) {
+      $newArr = [];
+      for($i = 0; $i <= count($arr); $i++) {
+          if($budget - $arr[$i] >= 0) {
+            $budget = $budget - $arr[$i];
+            array_push($newArr, $arr[$i]);
+          } else {
+            return $newArr;
+          }
+      }
+  }
 }
 
 class Action extends Arr {
@@ -253,7 +307,8 @@ $Misc->_space();
 echo $Arr->_values();
 $Misc->_space();
 $Arr->_sentence();
-echo $Arr->_shuffle();
+$Arr->_execute_quicksort(0, 6);
+
 $Misc->_double_space();
 $Arr->_sentence();
 
--- Technical-Interview/PHP-QuickSort.php
+++ Technical-Interview/PHP-QuickSort.php
@@ -7,20 +7,20 @@
           $this->arr = $arr;
       }
 
-      public function executeQuickSort($low, $high) {
+      public function _execute_quicksort($low, $high) {
 
           if($low===null || $high===null){
               return false;
           }
 
           if($low < $high) {
-              $pi = $this->partition($low, $high);
-              $this->executeQuickSort($low, $pi - 1);
-              $this->executeQuickSort($pi + 1, $high);
+              $pi = $this->_quicksort_partition($low, $high);
+              $this->_execute_quicksort($low, $pi - 1);
+              $this->_execute_quicksort($pi + 1, $high);
           }
       }
 
-      public function partition($low, $high) {
+      public function _quicksort_partition($low, $high) {
 
           if($low===null || $high===null){
               return false;
@@ -33,23 +33,23 @@
           for($incrementingVal = $low; $incrementingVal <= $high-1; $incrementingVal++) {
               if($this->arr[$incrementingVal] <= $pivot) {
                   $index++;
-                  $this->swap($index, $incrementingVal);
+                  $this->_quicksort_swap($index, $incrementingVal);
               }
           }
 
-          $this->swap($index+1,$high);
+          $this->_quicksort_swap($index+1,$high);
 
           return $index + 1;
       }
 
-      public function swap($index, $incrementingVal) {
+      public function _quicksort_swap($index, $incrementingVal) {
           $p=$this->arr[$index];
           $q=$this->arr[$incrementingVal];
           $this->arr[$index]=$q;
           $this->arr[$incrementingVal]=$p;
       }
 
-      public function checkBudget($arr, $budget) {
+      public function _quicksort_check_budget($arr, $budget) {
           $newArr = [];
           for($i = 0; $i <= count($arr); $i++) {
               if($budget - $arr[$i] >= 0) {
@@ -67,8 +67,8 @@
   $arr = [1, 12, 5, 111, 200, 1000, 10];
 
   $obj = new quickSort($arr);
-  $obj->executeQuickSort(0, 6);
+  $obj->_execute_quicksort(0, 6);
 
-  print_r($obj->checkBudget($obj->arr, $budget));
+  print_r($obj->_quicksort_check_budget($obj->arr, $budget));
   // print_r($obj->arr);
  ?>
