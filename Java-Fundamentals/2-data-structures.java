2d069f73a996c0efd6903b24d06e5f391f21b0b4
Emerson Pourghaed <ampourgh@gmail.com>
2018-11-24 19:39:32 -0600

    data structures cont

--- Java-Fundamentals/2-data-structures.java
+++ Java-Fundamentals/2-data-structures.java
@@ -17,49 +17,66 @@ arr1[2] = 3;
   ---------------------------------------------------------------
 */
 
+
 // ./Main.java
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 import com.team.Comment;
+import com.team.Comments;
+
 
 public class Main {
 
   public static void main(String[] args) {
+
     Comment comment = new Comment(
-      "craigsdennis",
-      "Want to be famous? Simply tweet about Java and use " +
-      "the hashtag #comment. I'll use your tweet in a new " +
-      "@treehouse course about data structures.",
+      "Emerson",
+      "Hello world.",
       new Date(1421849732000L)
     );
+
     Comment secondComment = new Comment(
-      "journeytocode",
-      "@treehouse makes learning Java sooooo fun! #comment",
-      new Date(1421878767000L)
+      "Emerson",
+      "Second comment goes here.",
+      new Date(1421849732000L)
     );
-    System.out.printf("This is a new Comment:  %s %n", comment);
-    System.out.println("The words are:");
-    for (String word: comment.getWords()) {
-      System.out.println(word);
+
+    Comment[] comments = Comments.load();
+    System.out.printf("There are %d comments. %n",
+                     comments.length);
+
+    Set<String> allHashTags = new HashSet<String>();
+    Set<String> allMentions = new TreeSet<String>();
+
+    for (Comment comment : comments) {
+      allHashTags.addAll(comment.getHashTags());
+      allMentions.addAll(comment.getMentions());
     }
-    Comment[] comments = {secondComment, comment};
-    Arrays.sort(comments);
-    for (Comment exampleComment : comments) {
-      System.out.println(exampleComment);
-    }
+
+    System.out.printf("Hash tags: %s %n", allHashTags);
+    System.out.printf("Mentions: %s %n", allMentions);
+
   }
 
 }
 
 
 // ./com/team/Comment.java
-
 package com.team;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
-public class Comment implements Comparable {
+public class Comment implements Comparable<Comment>, Serializable {
+
+  private static final long serialVersionUID = 7146681148113043748L;
   private String mAuthor;
   private String mDescription;
   private Date mCreationDate;
@@ -77,15 +94,18 @@ public class Comment implements Comparable {
   }
 
   @Override
-  public int compareTo(Object obj) {
-    Comment other = (Comment) obj;
+  public int compareTo(Comment other) {
+
     if (equals(other)) {
       return 0;
     }
+
     int dateCmp = mCreationDate.compareTo(other.mCreationDate);
+
     if (dateCmp == 0) {
       return mDescription.compareTo(other.mDescription);
     }
+
     return dateCmp;
   }
 
@@ -101,9 +121,70 @@ public class Comment implements Comparable {
     return mCreationDate;
   }
 
-  public String[] getWords() {
-   return mDescription.toLowerCase().split("[^\\w#@']+");
+  public List<String> getWords() {
 
+    String[] words = mDescription.toLowerCase().split("[^\\w#@']+");
+    return Arrays.asList(words);
+
+  }
+
+  public List<String> getHashTags() {
+    return getWordsPrefixedWith("#");
+  }
+
+  public List<String> getMentions() {
+    return getWordsPrefixedWith("@");
+  }
+
+  private List<String> getWordsPrefixedWith(String prefix) {
+
+    List<String> results = new ArrayList<String>();
+
+    for (String word : getWords()) {
+      if (word.startsWith(prefix)) {
+        results.add(word);
+      }
+    }
+
+    return results;
+  }
+
+}
+
+
+// ./com/team/Comments.java
+package com.team;
+
+import java.io.*;
+
+public class Comments {
+  public static void save(Comment[] comments) {
+    try (
+      FileOutputStream fos = new FileOutputStream("comments.ser");
+      ObjectOutputStream oos = new ObjectOutputStream(fos);
+    ) {
+      oos.writeObject(comments);
+    } catch(IOException ioe) {
+      System.out.println("Problem saving Comments");
+      ioe.printStackTrace();
+    }
+  }
+
+  public static Comment[] load() {
+    Comment[] comments = new Comment[0];
+    try (
+      FileInputStream fis = new FileInputStream("comments.ser");
+      ObjectInputStream ois = new ObjectInputStream(fis);
+    ) {
+      comments = (Comment[]) ois.readObject();
+    } catch(IOException ioe) {
+      System.out.println("Error reading file");
+      ioe.printStackTrace();
+    } catch(ClassNotFoundException cnfe) {
+      System.out.println("Error loading comments");
+      cnfe.printStackTrace();
+    }
+    return comments;
   }
 
 }
