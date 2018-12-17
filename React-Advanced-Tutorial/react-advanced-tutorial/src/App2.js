dc8e38e97a12168fb3ba4f4015d359c9f152afe7
Emerson Pourghaed <ampourgh@gmail.com>
2018-12-16 23:24:28 -0600

    practice (cont)

--- React-Advanced-Tutorial/react-advanced-tutorial/src/App2.js
+++ React-Advanced-Tutorial/react-advanced-tutorial/src/App2.js
@@ -4,11 +4,27 @@ import JSONdata from './data/data.json';
 import JSONcontent from './JSONcontent.js';
 
 class App2 extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // use when method is being called within your render
+    this.dataHandlerCallback = this.dataHandlerCallback.bind(this);
+  };
+
+  dataHandlerCallback(message) {
+    console.log(message);
+  }
+
   render() {
     return (
       <section>
         {JSONdata.map((JSONdetails, index) => {
-          return <JSONcontent post={JSONdetails} key={`post-list-key ${index}`} />
+          return <JSONcontent
+                  post={JSONdetails}
+                  key={`post-list-key ${index}`}
+                  dataCallback={this.dataHandlerCallback}
+                 />
         })}
       </section>
     );
--- React-Advanced-Tutorial/react-advanced-tutorial/src/JSONcontent.js
+++ React-Advanced-Tutorial/react-advanced-tutorial/src/JSONcontent.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 
 class JSONcontent extends Component {
+
+  constructor(props) {
+    super(props);
+    this.h1OnClick = this.h1OnClick.bind(this);
+  };
+
+  h1OnClick() {
+    // event.preventDefault();
+    const {dataCallback} = this.props;
+    dataCallback("Some message inserted here.");
+  }
+
   render() {
-
     const {post} = this.props;
-
     return (
       <div>
-        <h1>{post.title}</h1>
+        <h1 onClick={this.h1OnClick}>{post.title}</h1>
         <p>{post.detail}</p>
         <a href={`/id/${post.id}`} >Learn More</a>
-
       </div>
     );
   }
