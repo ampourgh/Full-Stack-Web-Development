8eaced2d4fc7e1354adc7e5fae2106c650408d8a
Emerson Pourghaed <ampourgh@gmail.com>
2018-12-23 23:43:53 -0600

    tutorial: refresher of vue.js

--- Vue-Refresher/app/app.js
+++ Vue-Refresher/app/app.js
@@ -3,12 +3,52 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Emerson',
+    age: 10,
     description: 'Nothingness and a half.',
-    url: 'http://www.nothingnothingnothing.org'
+    url: 'http://www.nothingnothingnothing.org',
+    webTag: '<a href="http://www.nothingnothingnothing.org">NothingNothingNothing</a>',
+    x: 0,
+    y: 0
   },
   methods: {
     greet: function(time) {
       return 'Good ' + time + ', ' + this.name;
+    },
+
+    add: function(inc) {
+      this.age+=inc;
+    },
+
+    subtract: function(dec) {
+      this.age-=dec;
+    },
+
+    updateXY: function(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    },
+
+    click: function() {
+      console.log('Cannot go to the website.');
+    }
+  }
+});
+
+
+// Single Vue Instance
+new Vue({
+  el: '#vue-second',
+  data: {
+    name: '',
+    age: ''
+  },
+  methods: {
+    logName: function() {
+      
+    },
+
+    logAge: function() {
+      console.log('You entered your age');
     }
   }
 });
