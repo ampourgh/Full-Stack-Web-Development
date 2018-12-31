8e1b7b5ad86f8c41ebba7cca0228c9868f1d2886
Emerson Pourghaed <ampourgh@gmail.com>
2018-12-30 22:31:30 -0600

    finish jsx section of react 2019

--- React-2019/jsx/src/index.js
+++ React-2019/jsx/src/index.js
@@ -1,12 +1,27 @@
+// Import React & ReactDOM - ES2015 import (not using Common.js require module system)
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Create React component
+const App = () => {
+
+  const enterValue = 'Enter your NAME here';
+
+  return (
+    <div>
+      {/* Hello world */}
+      <div className="awesome" style={{border: '1px solid red'}}>
+        <label htmlFor="name">Enter your name: </label>
+        <input type="text" id="name" />
+      </div>
+      <p>{ enterValue }</p>
+    </div>
+  );
+};
+
+// Take React component and show it on the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
