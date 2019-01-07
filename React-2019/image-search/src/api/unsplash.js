34e828c8fea9451adc20e85a1231f1ad2f5c153e
Emerson Pourghaed <ampourgh@gmail.com>
2019-01-06 18:35:55 -0600

    axios api setup and explanation

--- React-2019/image-search/src/api/unsplash.js
+++ React-2019/image-search/src/api/unsplash.js
@@ -1,9 +1,22 @@
+// Axios vs Fetch
+// Fetch is far more basic and requires you to do a lot of work that is built in with Axios
+// Fetch is built-in, Axios is third party that's installed through npm
+
+// npm install --save axios
+// >>> axios@0.18.0
 import axios from 'axios';
 
 export default axios.create({
+
+  // Getting the root url for the API's GET request
   baseURL: 'https://api.unsplash.com',
+
+  // In app.js
+  // params: { query: term }
+
   headers: {
     Authorization:
-      'Client-ID 34e39e5c2f447ce52009a515846ca2b6ccc35552bb63de59cf4a6d06728f3f7e'
+      // Client-ID <Access Key>
+      'Client-ID 1cd861d9b6548e96e1dcc3700249f976905f55009dec4700da13b0981f4ba197'
   }
 });
