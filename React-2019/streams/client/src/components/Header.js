24f4c231cc060ce80dbe0689c63b5d365992d187
Emerson Pourghaed <ampourgh@gmail.com>
2019-01-25 15:16:30 -0600

    React 2019: routers

---
+++ React-2019/streams/client/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
+
+const Header = () => {
+  return (
+    <div className="ui secondary pointing menu">
+
+      {/* Suggested method switch between routes
+          Do not use a href as it will reload the page, including the JavaScript
+        */}
+      <Link to="/" className="item">
+        Streamy
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All Streams
+        </Link>
+        <GoogleAuth />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
