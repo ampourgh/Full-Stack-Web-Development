896b152f387bb9de7b35de8bc578c4bca543aac5
Emerson Pourghaed <ampourgh@gmail.com>
2019-02-06 14:14:18 -0600

    car driving animation

---
+++ Animations/particles-img/README.md
@@ -0,0 +1,29 @@
+# particles.js
+
+![Giphy](https://media.giphy.com/media/xT9IgwJXQDlciFPExG/giphy.gif)
+
+
+A modification of particles.js to use custom icon images as particles.
+
+### Usage
+Include the particles-img.js file in your project, and add the following code.
+
+**index.html**
+```html
+<div id="particles-js"></div>
+
+<script src="particles.js"></script>
+```
+
+Specify image sources in img_src and configure particles.json to tweak settings.
+**app.js**
+```javascript
+/*Image source array*/
+let img_src = ['assets/images/001-interface.png', 'assets/images/002-share.png', 'assets/images/003-key.png','assets/images/004-front.png','assets/images/005-reception.png','assets/images/006-bed.png','assets/images/007-hotel.png','assets/images/008-room.png','assets/images/009-like.png'];
+
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+```
---
+++ Animations/particles-img/assets/js/app.js
@@ -0,0 +1,117 @@
+// Include images
+let img_src = [
+  'assets/images/car-r-2.svg',
+  'assets/images/car-r.svg',
+  'assets/images/truck.svg'
+];
+
+// Name images included
+let image_type = img_src.map(function(cuurentEl, index){ return "image" + index});
+
+// Configure particles-js
+particlesJS('particles-js',
+  {
+    "particles": {
+      "number": {
+        "value": img_src.length, // No of images
+        "density": {
+          "enable": true,
+          "value_area": 500 // Specify area (Lesser is greater density)
+        }
+      },
+      "color": {
+        "value": "#5affd4"
+      },
+      "shape": {
+        "type":  image_type, // Add images to particle-js
+        "stroke": {
+          "width": 0,
+        },
+        "polygon": {
+          "nb_sides": 4
+        }
+      },
+      "opacity": {
+        "value": 0.3, // Adjust opactiy
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 100, // Adjust the image size
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 10,
+          "size_min": 40,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 200,
+        "color": "#ffffff",
+        "opacity": 1,
+        "width": 2
+      },
+      "move": {
+        "enable": true,
+        "speed": 1,   // Speed of particle motion
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 1000,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+);
